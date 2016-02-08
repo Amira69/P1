6f8a29afb0c12ff874733d6e2b423c9d52f97f60
amir690 <aat452@g.harvard.edu>
2016-02-08 20:42:05 +0330

    P1 2nd commmit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Amir Tabrizi - DWA15
+
+# Live ULR
+[Welcome](http://p1.amiratabrizi.com)
+
+## Description
+> I used few techniques of HTML,CSS,and JS in my page to highlight required skills in addition to available libraries and frameworks for using in next projects
+
+## Demo
+NA
+## Details for teaching team
+
+No login required
+
+## Outside code
+* Bootstrap [http://getbootstrap.com](http://getbootstrap.com)
+* JQuery [http://jquery.com](http://jquery.com)
--- index.php
+++ index.php
@@ -4,15 +4,18 @@
 	<title>Amir Tabrizi P1 - DWA15 Portfolio</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" type="image/x-icon" href="images/ext_web.png">
+	<!-- NOTE: link to stable
 
-	<!-- NOTE: link to css
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/2.3.2/css/bootstrap.min.css" type="text/css">
-	<link href="//netdna.bootstrapcdn.com/bootswatch/2.3.2/flatly/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" type="text/css">
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-glyphicons.css" rel="stylesheet">
+	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
   <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
--->
-<!-- NOTE:  site ref -->
-  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	-->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
 	<link href="css/main.css" rel="stylesheet" type="text/css">
+<!-- NOTE:  assets ref -->
 
 
 </head>
@@ -22,10 +25,10 @@
 		<img src="images/photo.jpg" alt="Amir Tabrizi">
     <div class="row-fluid">
       <div class="span6 offset3">
-         <h3>About the Job</h3>
+         <h3>About The Job</h3>
 				 <p>
 					I love to develop application and my experience of programming is well appreciated when
-					I can share code with others, by teamwork of dynamic web applications they have timetables before
+					I can share code with others, by teamwork of dynamic web applications they have timetable before
 					enterprising a release so that everything is similar to software industry which modify our lives through
 					a true image of commitment. I learn with my head for the sake of head however most of the things are
 					achieved by body to mimic each other underground. Look over a framework ready to deploy different
@@ -35,7 +38,7 @@
     </div>
 
   <!-- NOTE:  carousel -->
-    <div id="myCarousel" class="carousel slide">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
       <ol class="carousel-indicators">
         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="1" class=""></li>
@@ -45,7 +48,13 @@
         <div class="item active">
           <img src="images/p2.jpg" alt="Proj2">
           <div class="carousel-caption">
-            <h4><a href="">Second Project  </a></h4>
+							<h4>
+								<?php // NOTE: 2nd Ref link?>
+								<a href=""> Second Project </a>
+								<!-- // NOTE: Github ref -->
+								<a href=""><i class="glyphicon">
+									<img class="img-responsive" src="images/GitHub-Mark-32pxi.png" alt="Github logo" /> </i></a>
+							</h4>
             <ul>
               <li>Form inputs to specify:</li>
               <ul>
@@ -55,12 +64,20 @@
               </ul>
               <li>Basic server-side validation (for example, what happens if the user enters a string instead of a number to indicate number of words?). You can do client-side validation as well, but it is not a substitute for server-side validation.</li>
             </ul>
-          </div>
+
+			  </div>
         </div>
         <div class="item">
           <img src="images/p3.jpg" alt="">
           <div class="carousel-caption">
-            <h4><a href="">Third Project  </a> </h4>
+
+						<!-- // NOTE: Github ref -->
+						<a href=""><i class="glyphicon">
+							<img class="img-responsive" src="images/GitHub-Mark-32pxi.png" alt="Github logo" /> </i></a>
+
+						<?php // NOTE: 2nd Ref link?>
+            <h4><a href=""> Third Project  </a> </h4>
+
             <ul>
               <li>Creating a new Laravel app.</li>
               <li>Installing and using Composer packages.</li>
@@ -73,7 +90,14 @@
         <div class="item">
           <img src="images/p4.jpg" alt="">
           <div class="carousel-caption">
-            <h4><a href="">Fourth Project  </a> </h4>
+
+						<!-- // NOTE: Github ref -->
+						<a href=""><i class="glyphicon">
+							<img class="img-responsive" src="images/GitHub-Mark-32pxi.png" alt="Github logo" /> </i></a>
+
+						<?php // NOTE: 2nd Ref link?>
+            <h4><a href=""> Fourth Project  </a> </h4>
+
             <ul>
               <li>Uses PHP/Laravel.</li>
               <li>Uses a database with at least 2 tables. This count does not include a users table, but does include pivot tables.</li>
@@ -90,9 +114,7 @@
 
   </div>
 
-
-  <script src="http://code.jquery.com/jquery.js"></script>
-
-	<script src="js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
 </body>
 </html>
